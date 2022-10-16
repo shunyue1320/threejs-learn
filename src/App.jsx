99b6f1af29144226863890b057b339a2255d6910
shunyue1320 <tsunyue1320@gmail.com>
2022-10-16 17:17:36 +0800

    chore: 添加光源并更换成受光照影响的材质

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,21 @@ function App() {
 
     // 创建一个立方缓冲几何体
     const geometry = new THREE.BoxGeometry(100, 100, 100);
-    // 创建材质
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // 创建材质 ( MeshBasicMaterial 材质不受光照影响，换成 MeshLambertMaterial 材质受光照影响)
+    const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
     // 生成带有材质的物体
     const cube = new THREE.Mesh(geometry, material);
     // 把物体添加进场景中
     scene.add(cube);
+
+    // 创建光源
+    // 环境光，没有特定的方向
+    const ambientLight = new THREE.AmbientLight(0x444444);
+    scene.add(ambientLight);
+    // 平行光，类似于生活中的太阳光
+    const directionalLight = new THREE.DirectionalLight(0x00ff00, 1);
+    directionalLight.position.set(400, 200, 300);
+    scene.add(directionalLight);
 
     /** 相机设置 */
     const width = window.innerWidth; //窗口宽度
